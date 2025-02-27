e86b5b53a587bb09e451b82325f48e41fd520e0e
haark200 <140924803+haark200@users.noreply.github.com>
2025-02-26 23:30:26 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Store API keys
 const SPOTIFY_CLIENT_ID = '697e57ae1f284c5ebdcdd1bbe0e3c83b';
 const SPOTIFY_CLIENT_SECRET = '5647c0ae61d04d00970869969add617b';
-const OPENAI_API_KEY = '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';
+const OPENAI_API_KEY = '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';
